1f39c8b442d7a357d6f6ece2c9698b3b9151ea0c
crysadm <mcomein@163.com>
2017-04-24 15:58:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,32 +4,32 @@
 
 #更新源
 
-apt-get update 
+sudo apt-get update 
 
 #安装python3.4
 
-apt-get install -y python3.4 python3.4-dev 
+sudo apt-get install -y python3.4 python3.4-dev 
 
 #安装pip，确保本脚本和get-pip.py 文件在一个文件夹
 BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-chmod +x ${BASE_DIR}/get-pip.py
-python3.4 ${BASE_DIR}/get-pip.py
+sudo chmod +x ${BASE_DIR}/get-pip.py
+sudo python3.4 ${BASE_DIR}/get-pip.py
 
-pip3.4 install redis && pip3.4 install requests && pip3.4 install flask && pip3.4 install flask-mail
+sudo pip3.4 install redis && sudo pip3.4 install requests && sudo pip3.4 install flask && sudo pip3.4 install flask-mail
 
 #安装redis-server
-apt-get install -y redis-server 
+sudo apt-get install -y redis-server 
 
 #添加树莓派开机启动
-cp -f crysadm_start /etc/init.d/crysadm_start
-chmod 755 /etc/init.d/crysadm_start
-update-rc.d crysadm_start defaults
+sudo cp -f crysadm_start /etc/init.d/crysadm_start
+sudo chmod 755 /etc/init.d/crysadm_start
+sudo update-rc.d crysadm_start defaults
 
 #运行云监工
 
-chmod +x ${BASE_DIR}/run.sh && chmod +x ${BASE_DIR}/down.sh  
-sh ${BASE_DIR}/run.sh
+sudo chmod +x ${BASE_DIR}/run.sh && sudo chmod +x ${BASE_DIR}/down.sh  
+sudo sh ${BASE_DIR}/run.sh
 
-#python3.4 ${BASE_DIR}/crysadm/crysadm/crysadm_helper.py &
-#python3.4 ${BASE_DIR}/crysadm/crysadm/crysadm.py &
+#sudo python3.4 ${BASE_DIR}/crysadm/crysadm/crysadm_helper.py &
+#sudo python3.4 ${BASE_DIR}/crysadm/crysadm/crysadm.py &
 
